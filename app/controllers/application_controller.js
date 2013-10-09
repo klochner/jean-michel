439794b9c9552de25e75df76ff918c2e3c31d148
gregory <greg2502@gmail.com>
2013-10-09 10:44:13 -0700

    update content

--- app/controllers/application_controller.js
+++ app/controllers/application_controller.js
@@ -1,7 +1,7 @@
 var actions = {};
 
 actions.index = function(req, res){
-  res.render('index', {title: 'My super dummy project'});
+  res.render('index', {title: 'Jean-michel ... coming soon'});
 }
 
 module.exports = actions;
--- public/scripts/controllers/dummyCtrl.js
+++ public/scripts/controllers/dummyCtrl.js
@@ -1,5 +1,5 @@
 define(['controllers/controllers'], function(controllers){
   controllers.controller('dummyCtrl', ['$scope', function($scope){
-    $scope.foo = 'Dummy Project';
+    $scope.foo = 'Jean-michel will help you keep track of your ping-pong matches and scores';
   }]);
 });
